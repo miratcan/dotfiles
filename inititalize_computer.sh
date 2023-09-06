68cf3225254b4c205f251dc76a8be75faf0046ee
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2023-09-06 09:32:44 +0300

    Create inititalize_computer.sh

---
+++ inititalize_computer.sh
@@ -0,0 +1,19 @@
+# Install XCode Command Line Tools
+xcode-select --install
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Nerd Fonts
+brew tap homebrew/cask-fonts
+brew search '/font-.*-nerd-font/' | awk '{ print $1 }' | xargs -I{} brew install --cask {} || true
+
+# Install Necessarry Homebrew Packages for Development
+brew install fd ripgrep opera iterm2 docker docker-compose btop nvm pyenv pipx zsh zsh-completions wget curl tmux ranger
+
+# Install Kaydet
+pip3 install kaydet
+
+# Install NVChad
+rm -rf ~/.config/nvim
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
